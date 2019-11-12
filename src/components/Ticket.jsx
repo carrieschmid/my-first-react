dbd6aba0655e11bfc293cec5c74ee26f109a48e7
Student <example@example.com>
2019-11-12 10:30:29 -0800

    Add more jsx pages to project and looping.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+  
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Ticket(){
+
+function Ticket(propsany){
   return (
-      <div>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase entries not saving!</em></p>
-        <hr/>
-      </div>
-   );
+    <div>
+      <h3>{propsany.location} - {propsany.names}</h3>
+      <p><em>{propsany.issue}</em></p>
+      <hr/>
+    </div>
+  );
 }
+
+// Ticket.propTypes = {
+//   names: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   issue: PropTypes.string
+// };
 
 export default Ticket;
